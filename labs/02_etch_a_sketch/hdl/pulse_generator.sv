0260c9f520d88fc7c31142b17a0ead67b777f211
Jacob Smilg <jacob@smilg.dev>
2022-11-08 10:35:09 -0500

    complete lab part 1

--- labs/02_etch_a_sketch/hdl/pulse_generator.sv
+++ labs/02_etch_a_sketch/hdl/pulse_generator.sv
@@ -12,5 +12,18 @@ output logic out;
 logic [N-1:0] counter;
 logic counter_comparator;
 
+always_comb counter_comparator = counter >= ticks;
+always_comb out = ena & counter_comparator;
+
+always_ff @(posedge clk) begin : pulse_gen_counter
+    if (rst) counter <= 0;
+    else if (ena) begin
+        if (counter_comparator) begin
+            counter <= 0;
+        end else begin
+            counter <= counter + 1;
+        end
+    end
+end
 
 endmodule
--- labs/02_etch_a_sketch/hdl/triangle_generator.sv
+++ labs/02_etch_a_sketch/hdl/triangle_generator.sv
@@ -9,5 +9,22 @@ output logic [N-1:0] out;
 typedef enum logic {COUNTING_UP, COUNTING_DOWN} state_t;
 state_t state;
 
+always_ff @(posedge clk) begin
+    if (rst) begin
+        out <= 0;
+        state <= COUNTING_UP;
+    end else if (ena) begin
+        case (state)
+            COUNTING_UP: begin
+                out <= out + 1;
+                if(&out[N-1:1] & ~out[0]) state <= COUNTING_DOWN;
+            end
+            COUNTING_DOWN : begin
+                out <= out - 1;
+                if(&({~out[N-1:1],out[0]})) state <= COUNTING_UP;
+            end
+        endcase
+    end
+end
 
 endmodule
